fee939fcf2c7ee0da4fa64c8d5e58d4f09b75827
chrisbarba123 <chris.barba@bjss.com>
2023-12-01 12:10:29 +0000

    first try of dayOne

--- day1/dayOne.ts
+++ day1/dayOne.ts
@@ -1,18 +1,26 @@
-const fetchRequest = async (): Promise<string> => {
-  const data: any = await fetch('https://adventofcode.com/2023/day/1/input', {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      Cookie:
-        '_ga=GA1.2.216317825.1701182660; _gid=GA1.2.863336555.1701422234; ru=53616c7465645f5f282179c75c316ab2dd36326df1d7535ab4cc9d987c3dd9de19dec355111aceade562a1c6299ff677; session=53616c7465645f5f2866e99d37b18c41dd25635901d1ba72ca4530b5edfd314aa04931b21af694a127a60ec6345ce3133ab63760390c5caac1236c09401b97d4; _ga_MHSNPJKWC7=GS1.2.1701422234.2.1.1701422596.0.0.0',
-    },
-  }).then((response) => response.text());
-  return data;
-};
+import Parser from '../src/dayParser';
 
 const displaySolution = async (): Promise<void> => {
-    let input : Promise<string> = fetchRequest().then(result => result);
-    let inputArray: string[] = (await input).split("\n")
+  const data: string = await Parser(1);
+  const dataArray: string[] = data.split('\n');
+  const output: (string | null)[] = dataArray
+    .map((item: string) => item.match(/[0-9]/g) as (string | null)[])
+    .reduce(arrayReducer);
+  console.log(output[0]);
 };
 
-displayText();
+const arrayReducer = (
+  a: (string | null)[],
+  b: (string | null)[]
+): (string | null)[] => {
+  const firstElement = (a as string[]) ?? ['0'];
+  const secondElement = (b as string[]) ?? ['0'];
+  return [
+    (
+      parseInt(firstElement[0] + firstElement[firstElement.length - 1]) +
+      parseInt(secondElement[0] + secondElement[secondElement.length - 1])
+    ).toString(),
+  ];
+};
+
+displaySolution();
---
+++ src/dayParser.ts
@@ -0,0 +1,16 @@
+const Parser = async (day: number): Promise<string> => {
+  const data: any = await fetch(
+    `https://adventofcode.com/2023/day/${day.toString()}/input`,
+    {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        Cookie:
+          '_ga=GA1.2.216317825.1701182660; _gid=GA1.2.863336555.1701422234; ru=53616c7465645f5f282179c75c316ab2dd36326df1d7535ab4cc9d987c3dd9de19dec355111aceade562a1c6299ff677; session=53616c7465645f5f2866e99d37b18c41dd25635901d1ba72ca4530b5edfd314aa04931b21af694a127a60ec6345ce3133ab63760390c5caac1236c09401b97d4; _ga_MHSNPJKWC7=GS1.2.1701422234.2.1.1701422596.0.0.0',
+      },
+    }
+  ).then((response) => response.text());
+  return data;
+};
+
+export default Parser;
