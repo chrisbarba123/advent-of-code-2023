dbe49354bc53214850e8ac714fe754020ffb3009
chrisbarba123 <chris.barba@bjss.com>
2023-12-01 12:42:56 +0000

    correct solution

--- day1/dayOne.ts
+++ day1/dayOne.ts
@@ -3,24 +3,24 @@ import Parser from '../src/dayParser';
 const displaySolution = async (): Promise<void> => {
   const data: string = await Parser(1);
   const dataArray: string[] = data.split('\n');
-  const output: (string | null)[] = dataArray
-    .map((item: string) => item.match(/[0-9]/g) as (string | null)[])
-    .reduce(arrayReducer);
-  console.log(output[0]);
+  console.dir(
+    dataArray
+      .map((item: string) => item.match(/[0-9]/g) as (string | null)[])
+      .filter((x) => x !== null)
+      .map(mapperData)
+      .reduce((a, b) => (parseInt(a!) + parseInt(b!)).toString()),
+    { maxArrayLength: null }
+  );
 };
 
-const arrayReducer = (
-  a: (string | null)[],
-  b: (string | null)[]
-): (string | null)[] => {
-  const firstElement = (a as string[]) ?? ['0'];
-  const secondElement = (b as string[]) ?? ['0'];
-  return [
-    (
-      parseInt(firstElement[0] + firstElement[firstElement.length - 1]) +
-      parseInt(secondElement[0] + secondElement[secondElement.length - 1])
-    ).toString(),
-  ];
+const mapperData = (item: (string | null)[]): string | undefined => {
+  const length = item.length ?? 0;
+  const elementOne = item[0];
+  const elementTwo = item[length - 1];
+  if (typeof elementOne === 'string' && typeof elementTwo === 'string') {
+    return elementOne.concat(elementTwo);
+  }
+  return undefined;
 };
 
 displaySolution();
