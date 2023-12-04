e88876a96e226445c1535c7e4d9036dd50c008e4
chrisbarba123 <chris.barba@bjss.com>
2023-12-04 13:23:52 +0000

    add correct solution (albeit messy and nasty) to day three part one

--- day3/dayThree.ts
+++ day3/dayThree.ts
@@ -1,26 +1,64 @@
 import Parser from '../utils/dayParser';
 
+const dotLength: string[] = ['.', '..', '...'];
+
 const displaySolution = async (): Promise<void> => {
   const data: string = await Parser(3);
-  const regexInline: RegExp = /(?<=[^.0-9])\d{1,3}|\d{1,3}(?=[^.0-9])/g;
+  const dataLine: string[] = data
+    .split('\n')
+    .filter((line: string) => line !== '');
+  const regexInline: RegExp = /(?<=[^.0-9])\d{1,3}|\d{1,3}(?=[^.0-9])/;
   const regexAllNumbers: RegExp = /\d{1,3}/g;
+  const symbolRegex: RegExp = /[^.0-9]/;
   let fullSum: number = 0;
-  let numberArray: string[] = data
-    .match(regexAllNumbers)
-    ?.filter((number: string | null) => number !== '') as string[];
-  const numberInlineWithSymbol: string[] = data.match(regexInline) as string[];
-  numberArray = numberArray.filter(
-    (number: string) => !numberInlineWithSymbol.includes(number)
-  );
+  let lineMatch: string[][] = [];
 
-  numberInlineWithSymbol.forEach((number: string) => {});
+  for (let i: number = 0; i < dataLine.length; i++) {
+    lineMatch.push(dataLine[i].match(regexAllNumbers) as string[]);
+    let line = dataLine[i];
+    for (let j: number = 0; j < lineMatch[i].length; j++) {
+      let regexInline: RegExp = new RegExp(
+        '(?<=[^.0-9])' + lineMatch[i][j] + '|' + lineMatch[i][j] + '(?=[^.0-9])'
+      );
+      const idx: number = line.indexOf(lineMatch[i][j]);
+      console.log(dataLine[i].substring(idx, idx + lineMatch[i][j].length));
+      const test: boolean = regexInline.test(dataLine[i]);
+      line = line.replace(
+        lineMatch[i][j],
+        dotLength[lineMatch[i][j].length - 1]
+      );
+      if (test) {
+        fullSum += parseInt(lineMatch[i][j]);
+        continue;
+      }
+      if (i - 1 !== -1) {
+        const beforeTest: boolean = symbolRegex.test(
+          dataLine[i - 1].substring(
+            Math.max(0, idx - 1),
+            Math.min(idx + lineMatch[i][j].length + 1, dataLine[i].length - 1)
+          )
+        );
+        if (beforeTest) {
+          fullSum += parseInt(lineMatch[i][j]);
+          continue;
+        }
+      }
+      if (i + 1 !== dataLine.length) {
+        const afterTest: boolean = symbolRegex.test(
+          dataLine[i + 1].substring(
+            Math.max(0, idx - 1),
+            Math.min(idx + lineMatch[i][j].length + 1, dataLine[i].length - 1)
+          )
+        );
+        if (afterTest) {
+          fullSum += parseInt(lineMatch[i][j]);
+          continue;
+        }
+      }
+    }
+  }
 
-  const allNumbers: string[] = data.match(/\d{1,3}/g) as string[];
-  console.log(allNumbers);
+  console.dir(fullSum, { maxArrayLength: null });
 };
 
 displaySolution();
-
-// data.split('\n').forEach((line : string) => {
-//   const numAndSymbol : string[] =
-// });
