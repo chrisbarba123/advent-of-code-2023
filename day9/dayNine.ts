c051a88760140b3873c6a78577140104f0f1263a
chrisbarba123 <chris.barba@bjss.com>
2023-12-11 09:56:53 +0000

    correct solution day nine part one

---
+++ day9/dayNine.ts
@@ -0,0 +1,37 @@
+import Parser from '../utils/dayParser';
+
+const displaySolution = async (): Promise<void> => {
+  const data: string = await Parser(9);
+  // const data: string = '0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45';
+  const dataSplit: number[][] = data
+    .split('\n')
+    .filter((line: string) => line !== '')
+    .map((line: string) =>
+      line.split(' ').map((number: string) => parseInt(number))
+    );
+  let answer: number = 0;
+  for (let line of dataSplit) {
+    answer += findNextPrediction(line);
+  }
+  console.log(answer);
+};
+
+const findNextPrediction = (line: number[]): number => {
+  console.log(line);
+  const difference: number[] = [];
+  let idx: number = 0;
+  while (idx !== line.length - 1) {
+    difference.push(line[idx + 1] - line[idx]);
+    idx++;
+  }
+  console.log(difference);
+  if (difference.every((number: number) => number === 0)) {
+    return line[line.length - 1];
+  }
+  return line[line.length - 1] + findNextPrediction(difference);
+};
+
+// const initialNext = (allDifference: number[]): number => {
+//   return 0;
+// };
+displaySolution();
